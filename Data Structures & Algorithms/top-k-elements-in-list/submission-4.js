33284b05e07f9d0cc62201de909db7a64e00d328
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2026-07-21 21:09:18 -0400

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+          let top = new Map();
+    for(let i = 0; i < nums.length; i += 1){
+        let currentEle = nums[i];
+
+        if(top.has(currentEle)){
+           top.set(currentEle,top.get(currentEle) + 1);
+        } else {
+            top.set(currentEle,1)
+        }
+    }
+    return Array.from(top).sort((a, b) => b[1] - a[1]).slice(0,k).map(pair => pair[0])
+    }
+}
