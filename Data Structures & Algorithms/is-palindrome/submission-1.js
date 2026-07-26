4f8b35af3ccce4214ae2be6bbb67ad29d52f9493
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2026-07-26 16:28:57 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+         let left = 0;
+    let right = s.length - 1;
+
+    while(left < right){
+        if(s[left].match(/[^a-zA-Z0-9]/)){
+            left++
+        } else if(s[right].match(/[^a-zA-Z0-9]/)) {
+           right--
+        } else{
+            if (s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            } 
+        left++;
+        right--;
+        } 
+        
+    }
+     return true
+    }
+}
